8fdf0fde466c50ad0bd1b382598e774493ae4f78
Eliyev Kenan <tu7gbzmx6@code.edu.az>
2022-04-08 11:39:19 +0400

    ConsoleApp79

---
+++ ConsoleApp79/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp79
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            bool choice=true;
+            while (choice)
+            {
+                Console.WriteLine("-------------Menu-----------");
+                Console.WriteLine("Secim edin!");
+                Console.WriteLine("1. Tələbə əlavə et");
+                Console.WriteLine("2. Tələbəyə imtahan əlavə et");
+                Console.WriteLine("3. Tələbənin bir imtahan balına bax");
+                Console.WriteLine("4. Tələbənin bütün imtahanlarını göstər");
+                Console.WriteLine("5. Tələbənin imtahan ortalamasını göstər");
+                Console.WriteLine("6. Tələbədən imtahan sil");
+                Console.WriteLine("0. Proqramı bitir");
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "1":
+                        Console.WriteLine("Full Name Daxil edin:");
+                        string fullName=Console.ReadLine();
+                        Student student = new Student()
+                        {
+                            FullName = fullName
+                        };
+                        students.Add(student);
+                        break;
+                        case "2":
+                        examName = StringInput("Exam Name :");
+                        double point = DoubleInput("Point :", "Point double olsun :");
+                        Student = students.Find(std => std.No == no);
+                        if (Student != null)
+                            Student.AddExam(examName, point);
+                        break;
+                        case "3":
+                        if (Student != null)
+                        {
+                            Console.WriteLine($"{no} -nomreli telebenin {examName} -adli imtahan neticesi :");
+                            Console.WriteLine(Student.GetExamResult(examName));
+                        }
+                        break;
+                    case "0":
+                        choice = false;
+                        break;
+                    default:
+                        break;
+                }
+                static string StringInput(string message)
+                {
+                    Console.WriteLine(message);
+                    string inputStr = Console.ReadLine();
+                    return inputStr;
+                }
+               
+            }
+        }
+    }
+}
---
+++ ConsoleApp79/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp79
+{
+    public class Student
+    {
+        public string FullName { get; set; }
+        public Dictionary<string, double> Exams=new Dictionary<string, double>();
+        private static int _no;
+        public int No { get; }
+        public Student()
+        {
+            _no++;
+            No = _no;
+        }
+        public double GetExamResult(string ExamName)
+        {
+            if (!string.IsNullOrEmpty(ExamName))
+            {
+                if (Exams.ContainsKey(ExamName))
+                {
+                    return Exams[ExamName];
+                }
+                throw new Exception("Xeta");
+            }
+            else
+            {
+                throw new Exception("Xeta!");
+            }
+        }
+        public void AddExam(string examName,double point)
+        {
+            if (!string.IsNullOrEmpty(examName) && point>=0 && point<=100)
+            {
+                Exams.Add(examName,point);
+            }
+            else
+            {
+                throw new Exception("Xeta!");
+            }
+        }
+        public double GetExamAvg()
+        {
+            double sum = 0;
+            foreach (var item in Exams)
+            {
+                sum = sum + item.Value;
+            }
+            return sum;
+        }
+    }
+}
